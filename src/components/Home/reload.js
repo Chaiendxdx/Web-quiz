ac62bee9ff7d450a850a652527a0f131b57c59b2
Chaiendxdx <92135703+Chaiendxdx@users.noreply.github.com>
2022-12-23 22:33:19 +0700

    add jsonserver (#2)

--- src/components/Home/reload.js
+++
@@ -1,8 +0,0 @@
-import { useDispatch } from "react-redux";
-import { reload } from "../../redux/action/userAction";
-
-const Reload = () => {
-  const dispatch = useDispatch();
-};
-
-export default Reload;
